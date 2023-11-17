bd68b2dff6362bca1aa8e19ae680ee81f5b52afe
KustelR <dmitriysstudios2@gmail.com>
2023-11-17 16:08:11 +0400

    Changed header and moved it to layout

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,8 +1,16 @@
+import Link from "next/link";
+
 type Props = {}
 
 export default function Header(props: Props) {
     const {} = props;
   return (
-    <div className='w-full bg-red-600 text-white'>Header</div>
+    <div className='bg-red-600 px-2 text-white items-end md:flex text-xl'>
+      <h1 className="text-3xl font-bold mr-4">PAINT<br />ONLINE</h1>
+      <ul className="md:flex">
+        <li className="mr-2"><Link href="/">Home</Link></li>
+
+      </ul>
+    </div>
   )
 }
