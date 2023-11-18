f900aec35af604c9a89ca189908c02844055bb29
KustelR <dmitriysstudios2@gmail.com>
2023-11-18 20:58:28 +0400

    added history class

--- frontend/src/scripts/drawSerializer.ts
+++ frontend/src/scripts/drawSerializer.ts
@@ -20,6 +20,45 @@ export class DrawData {
     }
 }
 
+export class History {
+    constructor(limit: number | undefined) {
+        this.actions = [];
+        if (limit) this.limit = limit;
+    }
+
+    limit: number = 20
+    actions: Array<DrawData>;
+    offset: number = 0;
+
+    addAction(action: DrawData) {
+        this.actions.push(action);
+        if (this.actions.length > this.limit) {
+            this.actions = this.actions.slice(this.actions.length - this.limit)
+        }
+    }
+
+    remind() {
+        return this.actions.slice(0, this.actions.length - (1 + this.offset));
+    }
+
+    undo() {
+        if (this.offset  < this.actions.length) {
+            this.offset++;
+        } else {
+            throw new Error("NOthing to undo")
+        }
+    }
+
+    redo() {
+        if (this.offset > 0) {
+            this.offset--;
+        } else {
+            throw new Error("Nothing to redo")
+        }
+    }
+
+}
+
 type action = {
     start?: Position
     end?: Position
