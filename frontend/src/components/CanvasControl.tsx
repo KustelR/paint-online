8141aa77168d31889393f8689ebca02945525d91
KustelR <dmitriysstudios2@gmail.com>
2023-11-21 13:42:53 +0400

    Moved canvas controls into separated file

---
+++ frontend/src/components/CanvasControl.tsx
@@ -0,0 +1,76 @@
+import React from "react";
+
+type Props = {
+  colorSetter: (color: string) => void;
+  lineWidthSetter: (width: number) => void;
+  drawModeSetter: (mode: number) => void;
+  undo: () => void;
+  redo: () => void;
+};
+
+const PencilDrawMode = 0;
+const LineDrawMode = 1;
+
+export default function CanvasControl({
+  colorSetter,
+  lineWidthSetter,
+  drawModeSetter,
+  undo,
+  redo,
+}: Props) {
+  return (
+    <div className="px-2 m-1 border-l border-l-neutral-200">
+      <ul>
+        <li className="mb-4">
+          <h3 className="font-bold">Draw mode</h3>
+          <ul>
+            <li>
+              <button onClick={() => drawModeSetter(PencilDrawMode)}>
+                Pencil
+              </button>
+            </li>
+            <li>
+              <button onClick={() => drawModeSetter(LineDrawMode)}>
+                Line
+              </button>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <label className="block" htmlFor="color">
+            Color
+          </label>
+          <input
+            className="bg-neutral-100"
+            placeholder="#000000"
+            onChange={(e) => {
+              if (e.target.value.length < 7 || !e.target.value.startsWith("#"))
+                return;
+              colorSetter(e.target.value);
+            }}
+            name="color"
+            type="color"
+          />
+        </li>
+        <li>
+          <label htmlFor="line-width" className="block">
+            Line width
+          </label>
+          <input
+            name="line-width"
+            onChange={(e) => {
+              const w = Number(e.target.value);
+              lineWidthSetter(w);
+            }}
+            type="text"
+            className="bg-neutral-100"
+          />
+        </li>
+        <li className="flex justify-between">
+          <button onClick={undo}>{"Undo <-"}</button>
+          <button onClick={redo}>{"Redo ->"}</button>
+        </li>
+      </ul>
+    </div>
+  );
+}
